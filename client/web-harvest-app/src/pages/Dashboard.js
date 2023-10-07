542db86d672dbfcd1c4238a6535eab9f98307d6e
Ganesh Jha <ganeshjha804@gmail.com>
2023-10-07 20:27:23 +0530

    login&signup

---
+++ client/web-harvest-app/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+export default function Dashboard(){
+    return(
+        <>
+            <div>
+                <Navbar />
+                <div>
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+        </>
+    );
+}
