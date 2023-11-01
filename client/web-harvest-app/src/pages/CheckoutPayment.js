d77ba714b2c536d7164c3bcef1e54bf985f03b06
the_Sunny_Sharma_ <syn8433898723@gmail.com>
2023-11-01 14:56:26 +0530

    Completed Flow of Payment Procedure

--- client/web-harvest-app/src/pages/CheckoutPayment.js
+++ client/web-harvest-app/src/pages/CheckoutPayment.js
@@ -1,12 +1,66 @@
 import NavbarCheckout from "../components/NavbarCheckout";
 import { useNavigate } from "react-router-dom";
-export default function CheckoutPayment() {
-    const navigate = useNavigate();
-    return(<>
-        <NavbarCheckout />
-        <p>CAsh on Delhivery</p>
-        <button onClick={ () => {navigate('/')}}>PAY</button>
-        </>
+import '../styles/carditemco.css';
+import { useState } from "react";
 
-    );
+export default function CheckoutPayment() {
+  const navigate = useNavigate();
+    // Initialize a state variable to hold the selected value
+    const [payMode, setPayMode] = useState("cash"); // Default selection
+
+    // Handle changes in the radio button selection
+    const hPayMode = (event) => {
+      setPayMode(event.target.value);
+    };
+  return (
+    <>
+      <NavbarCheckout />
+      <div className="checkout-card-wapper co-summary">
+        <div class="container-fluid ">
+          <div class="row payMode">
+            <div class="col-12 mt-3">
+              <div class="card card-width">
+              <label className="co-label">
+            <input
+              type="radio"
+              value="cash"
+              checked={payMode === "cash"}
+              onChange={hPayMode}
+            />
+            Cash On Delhivery
+          </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+<div class="container-fluid payMode">
+          <div class="row">
+            <div class="col-12 mt-3">
+              <div class="card card-width">
+              <label className="co-label">
+            <input
+              type="radio"
+              value="onlinePay"
+              checked={payMode === "onlinePay"}
+              onChange={hPayMode}
+            />
+            Pay Online
+          </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+      className="checkout-btn payBtn"
+        onClick={() => {
+          navigate("/checkout");
+        }}
+      >
+        PAY
+      </button>
+    </>
+  );
 }
+
--- client/web-harvest-app/src/pages/CheckoutSummary.js
+++ client/web-harvest-app/src/pages/CheckoutSummary.js
@@ -1,10 +1,14 @@
+import CardItemCO from "../components/CartItemCO";
 import NavbarCheckout from "../components/NavbarCheckout";
 import { useNavigate } from "react-router-dom";
+import '../styles/carditemco.css';
+
 export default function CheckoutSummary() {
     const navigate = useNavigate();
     return(<>
         <NavbarCheckout />
-        <button onClick={ () => {navigate('/payment')}}>Proceed to Pay</button>
+        <CardItemCO />
+        <button className="checkout-btn co-pp" onClick={ () => {navigate('/payment')}}>Proceed to Pay</button>
         </>
 
     );
